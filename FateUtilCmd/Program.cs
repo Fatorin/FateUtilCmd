3d9c9733490c8a9de44d06ced541009e8feab3d0
Fatorin <sksly789@gmail.com>
2020-09-05 03:11:39 +0800

    更新投降系統插入

--- FateUtilCmd/Program.cs
+++ FateUtilCmd/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -18,8 +19,6 @@ namespace FateUtilCmd
 
         static void Main(string[] args)
         {
-            //Debug Used
-
             _jassFilePath = $"{_directoryPath}/war3map.j";
 
             if (!File.Exists(_jassFilePath))
@@ -30,10 +29,14 @@ namespace FateUtilCmd
             //初始化誤刪
             _str = File.ReadAllText(_jassFilePath);
             _sb = new StringBuilder(_str);
-            
 
-            AddRefeshScoreBoard();
-            AddSkin();
+
+            //AddRefeshScoreBoard();
+            //AddSkin();
+            AddSurrenderSystem();
+            File.WriteAllText(_jassFilePath, _str);
+            Console.WriteLine("任務已完成");
+            Console.ReadKey();
             /*string rgx1 = "Table__ht";
             string rgx2 = "Table___ht";
             AdjustPrefix(rgx1, rgx2);*/
@@ -41,9 +44,67 @@ namespace FateUtilCmd
 
         }
 
+        static void AddSurrenderSystem()
+        {
+            string stringPathFunc = $"{_directoryPath}/AddSurrenderSystemPart1.txt";
+            string stringPathFunc2 = $"{_directoryPath}/AddSurrenderSystemPart2.txt";
+            if (!File.Exists(stringPathFunc))
+            {
+                Console.WriteLine("AddSurrenderSystemPart1.txt 不存在，所以不修改");
+                return;
+            }
+
+            if (!File.Exists(stringPathFunc2))
+            {
+                Console.WriteLine("AddSurrenderSystemPart2.txt 不存在，所以不修改");
+                return;
+            }
+
+            TextForPosition obj = new TextForPosition();
+            //讀取變數
+            obj.CompareStringFirst = "endglobals";
+            obj.CompareStringSecond = "";
+            obj.ClearAtStringBefore = "";
+            obj.InsertString = File.ReadAllText(stringPathFunc);
+            AddCodeAfterSearchText(obj, true);
+
+            obj.CompareStringFirst = "call s__Game_start_hold()";
+            obj.CompareStringSecond = "";
+            obj.ClearAtStringBefore = "";
+            obj.InsertString = "set flag_surrender=true";
+            AddCodeAfterSearchText(obj, false);
+
+            obj.CompareStringFirst = "8000元以下的玩家會獲得3000元的支援。回合失敗方會多獲得500元的支援。";
+            obj.CompareStringSecond = "";
+            obj.ClearAtStringBefore = "";
+            obj.InsertString = "set flag_surrender=true\r" + "call EnableTrigger(Trg_SurrenderSystem)";
+            AddCodeAfterSearchText(obj, false);
+
+            obj.CompareStringFirst = "GoldTransfer__OnType";
+            obj.CompareStringSecond = "GoldTransfer___OnType";
+            obj.ClearAtStringBefore = "";
+            obj.InsertString = File.ReadAllText(stringPathFunc2);
+            AddCodeAfterSearchText(obj, true);
+
+            obj.CompareStringFirst = "call BootSystem__OnInit()";
+            obj.CompareStringSecond = "call BootSystem___OnInit()";
+            obj.ClearAtStringBefore = "";
+            obj.InsertString = "call SurrenderSystem__OnInit()";
+            AddCodeAfterSearchText(obj, false);
+
+        }
+
         static void AddRefeshScoreBoard()
         {
+            string stringPathFunc = $"{_directoryPath}/AddRefeshScoreBoardFunc.txt";
             string stringPath = $"{_directoryPath}/AddRefeshScoreBoard.txt";
+            string stringUpdateFunc = $"call UpdateTeamScoreBoard()";
+
+            if (!File.Exists(stringPathFunc))
+            {
+                Console.WriteLine("AddRefeshScoreBoardFunc.txt 不存在，所以不修改");
+                return;
+            }
 
             if (!File.Exists(stringPath))
             {
@@ -51,12 +112,26 @@ namespace FateUtilCmd
                 return;
             }
 
+            //補上Function
             TextForPosition obj = new TextForPosition();
             obj.CompareStringFirst = "s__BloodBath__DoT_periodic";
             obj.CompareStringSecond = "s__BloodBath___DoT_periodic";
             obj.ClearAtStringBefore = "";
-            obj.InsertString =File.ReadAllText(stringPath);
+            obj.InsertString = File.ReadAllText(stringPathFunc);
             AddCodeAfterSearchText(obj, true);
+
+            //替換成FUNCTION名稱
+            obj.InsertString = stringUpdateFunc;
+
+            using (StreamReader sr = new StreamReader(stringPath))
+            {
+                while (sr.Peek() >= 0)
+                {
+                    obj.CompareStringFirst = sr.ReadLine();
+                    obj.CompareStringSecond = sr.ReadLine();
+                    AddCodeAfterSearchText(obj, false);
+                }
+            }
         }
 
         static void AddSkin()
@@ -103,8 +178,8 @@ namespace FateUtilCmd
                 cleanStrStartPos = _str.IndexOf(_newLine, startPos) + _newLine.Length;
             }
 
-
-            if (cleanStrStartPos == -1)
+            //找不到是-1，+1之後變0
+            if (cleanStrStartPos == 0)
             {
                 Console.WriteLine($"{nameof(AddCodeAfterSearchText)}:找不到的換行位置");
                 return;
@@ -120,7 +195,6 @@ namespace FateUtilCmd
             _sb.Insert(cleanStrStartPos, obj.InsertString + _newLine);
 
             _str = _sb.ToString();
-            File.WriteAllText(_jassFilePath, _str);
         }
 
         static int CompareReutrnPosition(string compareString1, string compareString2)
@@ -169,7 +243,6 @@ namespace FateUtilCmd
             }
 
             _str = _sb.ToString();
-            File.WriteAllText(_jassFilePath, _str);
         }
     }
 }
